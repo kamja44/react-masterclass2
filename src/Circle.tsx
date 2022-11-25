07bb191fa59670abb557e1b1b40aebfbcfa2d0a8
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-11-25 13:23:24 +0900

    Optional Props
    interface optional로 만들기
    borderColor?: string

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,19 +1,30 @@
 import styled from "styled-components";
+import { StringLiteral } from "typescript";
 
+interface ContainerProps{
+    bgColor: string;
+    borderColor: string;
+}
 interface CircleProps{
     bgColor: string;
+    borderColor?: string;
+    text ?: string;
 }
 
-const Container = styled.div<CircleProps>`
+const Container = styled.div<ContainerProps>`
     width: 200px;
     height: 200px;
     background-color: ${(props) => props.bgColor};
     border-radius: 50%;
+    border: 1px solid ${props => props.borderColor};
 `
 
-function Circle({bgColor}: CircleProps){
+function Circle({bgColor, borderColor, text = "default Value"}: CircleProps){
     return(
-        <Container bgColor={bgColor}/>
+        //  ?? <- 기본값 설정
+        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}> 
+            {text}
+        </Container>
     );
 }
 
