f4479e6f0178e262a1c68dc8b722fa3b851c3db5
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-12-06 16:15:05 +0900

    setting Crypto Tracker
    npm i react-router-dom@5.3.0
    
    coinpaprika <- coin data
    
    npm i --save-dev @types/react-router-dom

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 styled.d.ts <- index.d.ts 파일을 확장하기 위해 사용
 즉, index.d.ts file 덮어쓰기한다.
+
+userParams <- URL에서 정보를 얻어올 수 있다.
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,6 @@
-import React from "react";
-import { useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
+import Router from "./router";
 function App() {
-  return (
-    <Container>
-      <H1>protected</H1>
-    </Container>
-  );
+  return <Router></Router>;
 }
 
 export default App;
